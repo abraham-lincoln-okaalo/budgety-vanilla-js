504adf14bfd444a3c6dda8dfc6ab92ceb9fb0f2c
lincoln <lincolnokaalo@gmail.com>
2021-05-13 13:56:52 +0300

    initial logic for UI controller

--- app.js
+++ app.js
@@ -4,9 +4,19 @@
 // update the UI
 var UIcontroller = (function(){
 
+ return{
+  getInput: function(){
+
+   return {
+     type: document.querySelector('.add__type').value,
+     description:  document.querySelector(".add__description").value,
+     value: document.querySelector(".add__value").value,
+   }
+  }
+ }
 })();
 
-// data module
+// budget controller module
 // add the new item to our data structure
 // calculate the budget
 var budgetController = (function () {
@@ -19,5 +29,18 @@ var budgetController = (function () {
 // controller module
 // add event handler
 var controller = (function(budgetCtrl, UICtrl){
+
+ var ctrlAddItem = function(){
+   // get field input data
+   // add item to budget controller
+   // add item ui
+   // calculcate the budget
+   // display the budget to the ui
+ }
+ document.querySelector('add__btn').addEventListener('click', ctrlAddItem);
+
+ document.querySelector('keypress', function(event){
+
+ });
 
 })(budgetController, UIcontroller)
