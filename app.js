bb833511e9ce1e81be589b23bbfc66adfbf1b34e
lincoln <lincolnokaalo@gmail.com>
2021-05-13 17:10:32 +0300

    cleaning the input fields

--- app.js
+++ app.js
@@ -117,6 +117,19 @@ var UIcontroller = (function () {
       // insert the html into the dom
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
+    clearFields: function(){
+     var fields, fieldsArr;
+
+     fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+     fieldsArr = Array.prototype.slice.call(fields);
+
+     fieldsArr.forEach((current, index, array) => {
+      current.value = "";
+
+     });
+     fieldsArr[0].focus();
+    },
 
     getDOMstrings: function () {
       return DOMstrings;
@@ -149,6 +162,10 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     // add item ui
     UICtrl.addListItem(newItem, input.type);
+
+    // clear fields
+    UICtrl.clearFields();
+
     // calculcate the budget
     // display the budget to the ui
   };
